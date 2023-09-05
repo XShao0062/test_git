d270835350b9b3798d09e2a6ac744c20ddb60299
shaoxi <shaoxi@LAPTOP-AEK1HANG.localdomain>
2023-09-05 09:39:53 +0800

    project after workshop day 1

---
+++ README.md
@@ -0,0 +1,10 @@
+##SkySim
+
+`sky_sim.py` is a script to simulate a sky catalogue, 
+it will produce a file named `catalog.csv`
+
+
+##Installing
+
+
+
---
+++ mymodule/__init__.py
@@ -0,0 +1,5 @@
+print(f"hello from {__name__}")
+
+def func():
+    print("You just ran the function called `func` from module `mymodule`")
+__all__ = ['sky_sim']
