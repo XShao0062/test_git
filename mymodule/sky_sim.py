bb0d40687eb2969f221a286775dede3722e407c8
XShao0062 <67062076+XShao0062@users.noreply.github.com>
2023-09-06 16:12:47 +0800

    Add configure the argparse for sky_sim.py

--- mymodule/sky_sim.py
+++ mymodule/sky_sim.py
@@ -107,12 +107,39 @@ def save_positions(ras, decs, out='catalog.csv'):
             print(f"{i:07d}, {ras[i]:12f}, {decs[i]:12f}", file=f)
     log.info(f"Wrote file {out}")
 
-def clip_to_radius():
-    return
+
+def skysim_parser():
+    """
+    Configure the argparse for skysim
+
+    Returns
+    -------
+    parser : argparse.ArgumentParser
+        The parser for skysim.
+    """
+    parser = argparse.ArgumentParser(prog='sky_sim', prefix_chars='-')
+    parser.add_argument('--ra', dest = 'ra', type=float, default=None,
+                        help="Central ra (degrees) for the simulation location")
+    parser.add_argument('--dec', dest = 'dec', type=float, default=None,
+                        help="Central dec (degrees) for the simulation location")
+    parser.add_argument('--out', dest='out', type=str, default='catalog.csv',
+                        help='destination for the output catalog')
+    return parser
+
 
 def main():
-    ras, decs = make_positions()
-    save_positions(ras, decs)
+    parser = skysim_parser()
+    options = parse.parse_args()
+
+     # if ra/dec are not supplied the use a default value
+    if None in [options.ra, options.dec]:
+        ra, dec = get_radec()
+    else:
+        ra = options.ra
+        dec = options.dec 
+        
+    ras, decs = make_positions(ra, dec, NSRC)
+    save_positions(ras, decs, options.out)
 
 
 if __name__ =="__main__":
